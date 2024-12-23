b896953dbe9af782f6192fbd7f886cccfaeefaf5
German <123388703+NasdormML@users.noreply.github.com>
2024-12-23 13:05:42 +0400

    Update model_prediction.py

--- src/model_prediction.py
+++ src/model_prediction.py
@@ -3,21 +3,13 @@ import joblib
 import xgboost as xgb
 
 def predict(test_data_path):
-    # Загрузка данных
     test = pd.read_csv(test_data_path)
-    
-    # Загрузка модели
     model, target_encoder, preprocessor = joblib.load("models/trained_model.pkl")
-
-    # Подготовка данных
     X_test_encoded = target_encoder.transform(test)
     X_test_processed = preprocessor.transform(X_test_encoded)
-    
     dtest = xgb.DMatrix(X_test_processed)
-    
-    # Предсказания
     predictions = model.predict(dtest)
     return predictions
 
 if __name__ == "__main__":
-    predict()
+    predict("data/processed/test.csv")
