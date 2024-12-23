7401e00b7c1e99a2e68343e0e320fa9d66faf314
German <123388703+NasdormML@users.noreply.github.com>
2024-12-23 13:05:36 +0400

    Update model_training.py

--- src/model_training.py
+++ src/model_training.py
@@ -7,16 +7,17 @@ import category_encoders as ce
 import os
 import joblib
 from sklearn.model_selection import train_test_split
+import json
 
-def train_model(train, categorical_features, numerical_features, params):
-    # Разделение данных
+def train_model(train, categorical_features, numerical_features):
+    with open("hyperparameters.json", "r") as f:
+        params = json.load(f)
+
     X = train.drop(columns=['sales', 'log_sales'])
     y = train['log_sales']
 
-    # Разделение на обучающую и валидационную выборки
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Кодирование и подготовка данных
     target_encoder = ce.TargetEncoder(cols=categorical_features)
     X_train_encoded = target_encoder.fit_transform(X_train, y_train)
     X_val_encoded = target_encoder.transform(X_val)
@@ -31,7 +32,6 @@ def train_model(train, categorical_features, numerical_features, params):
     X_train_processed = preprocessor.fit_transform(X_train_encoded)
     X_val_processed = preprocessor.transform(X_val_encoded)
 
-    # Обучение модели
     dtrain = xgb.DMatrix(X_train_processed, label=y_train)
     dval = xgb.DMatrix(X_val_processed, label=y_val)
 
@@ -43,25 +43,18 @@ def train_model(train, categorical_features, numerical_features, params):
         early_stopping_rounds=15
     )
 
-    # Создание папки models, если её нет
     if not os.path.exists('models'):
         os.makedirs('models')
 
-    # Сохранение модели
     model_path = os.path.join(os.getcwd(), "models", "trained_model.pkl")
     joblib.dump((model, target_encoder, preprocessor), model_path)
     return model
 
 if __name__ == "__main__":
     import pandas as pd
-    import json
 
     train_data = pd.read_csv("data/processed/train.csv")
     categorical_features = ['holiday_type', 'locale', 'locale_name', 'store_nbr', 'family', 'city', 'state', 'cluster']
     numerical_features = ['onpromotion', 'transactions', 'oil_price', 'lag_7_sales', 'lag_14_sales', 'rolling_mean_7', 'rolling_mean_14']
-    
-    # Загрузка гиперпараметров
-    with open("hyperparameters.json", "r") as f:
-        best_params = json.load(f)
 
-    train_model(train_data, categorical_features, numerical_features, best_params)
+    train_model(train_data, categorical_features, numerical_features)
